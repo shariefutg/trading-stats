b47aa222601af4b8aada83c043dc25cbc13eb434
shariefutg <72771645+shariefutg@users.noreply.github.com>
2023-05-18 18:39:35 -0400

    Update nyse-weekly-5-dollar-and-above-intraday-change.py

--- nyse-weekly-5-dollar-and-above-intraday-change.py
+++ nyse-weekly-5-dollar-and-above-intraday-change.py
@@ -1,3 +1,37 @@
+"""
+This Python script uses the yfinance, pandas, and re (Regular Expressions) libraries to analyze historical stock data from the New York Stock Exchange (NYSE). The function get_stocks_in_range now returns a list of stock tickers that not only meet specific closing price criteria over a specified period, but also have a certain minimum intraday price change.
+
+Here's the process it follows:
+
+The get_stocks_in_range function now takes six parameters:
+
+start_date and end_date to define the period for which the stock history is analyzed.
+min_price and max_price to set the range for the stock's closing price.
+min_intraday_change sets the minimum difference between the stock's highest and lowest price within a single day.
+pct is a percentage that specifies the portion of trading days where the intraday change must be at least min_intraday_change.
+The script first downloads a CSV file from a GitHub repository that contains a list of all NYSE stock symbols.
+
+Next, it filters the list of stock symbols, keeping only those that contain only alphanumeric characters.
+
+For each ticker in the filtered list, the script attempts to download the stock's historical data from Yahoo Finance using the yfinance library. If the data cannot be downloaded, the script moves on to the next ticker.
+
+For each downloaded stock history, it creates a new column, intraday_change, that contains the difference between the highest and lowest price each day.
+
+The script then checks if all of the closing prices fall within the range defined by min_price and max_price.
+
+If the above condition is met, it then checks if the intraday_change is at least min_intraday_change for a pct proportion of the trading days.
+
+If both conditions are met, the ticker is added to the stocks_in_range list.
+
+Finally, the function returns the stocks_in_range list.
+
+The last two lines of the code call the get_stocks_in_range function with specific parameters and print the resulting list of tickers.
+
+Business case:
+
+This function could be used by a trader or a trading firm to filter out stocks that show a certain level of volatility (measured by intraday price change) and also have closing prices within a certain range. This might be useful for strategies that aim to profit from intraday price movements. For instance, the code looks for stocks where the intraday price change is at least $5 for all trading days in the defined period, and the closing price has always been between $0 and $500,000 during the same period. The identified stocks may be suitable for certain types of intraday trading strategies, such as day trading or swing trading.
+"""
+
 import yfinance as yf
 import pandas as pd
 import re
