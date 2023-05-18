d8bab774ab328f5c45519fddabd150917a92d9cf
shariefutg <72771645+shariefutg@users.noreply.github.com>
2023-05-18 18:35:54 -0400

    Update nyse-weekly-positive-trend-daily.py

--- nyse-weekly-positive-trend-daily.py
+++ nyse-weekly-positive-trend-daily.py
@@ -1,3 +1,34 @@
+"""
+This Python script uses the yfinance, pandas, and re (Regular Expressions) libraries to analyze historical stock data from the New York Stock Exchange (NYSE). The function get_stocks_in_range returns a list of stock tickers that meet certain criteria over a specified period.
+
+Here's the process it follows:
+
+The get_stocks_in_range function takes five parameters:
+
+start_date and end_date define the period for which the stock history is analyzed.
+min_price and max_price set the range for the stock's closing price.
+pct is a percentage that specifies the portion of trading days in which the stock's closing price must exceed its opening price.
+The script first downloads a CSV file from a GitHub repository that contains a list of all NYSE stock symbols.
+
+Next, it filters the list of stock symbols, keeping only those that contain only alphanumeric characters.
+
+For each ticker in the filtered list, the script attempts to download the stock's historical data from Yahoo Finance using the yfinance library. If the data cannot be downloaded, the script moves on to the next ticker.
+
+For each downloaded stock history, it creates a new column, open_close_change, that contains the difference between the opening and closing price each day.
+
+The script then checks if all of the closing prices fall within the range defined by min_price and max_price.
+
+If the above condition is met, it then checks if the open_close_change is positive (i.e., the stock's price increased during the day) for at least a pct proportion of the trading days.
+
+If both conditions are met, the ticker is added to the stocks_in_range list.
+
+Finally, the function returns the stocks_in_range list.
+
+Business case:
+
+This function could be used by an investment firm or a financial advisor to filter out stocks that meet certain criteria for their clients or strategies. For instance, it can identify stocks that have consistently closed within a specific price range and demonstrated a certain level of growth (defined as the closing price being higher than the opening price) over a defined period. This can be useful for investors looking for stable stocks that show steady growth over time.
+"""
+
 import yfinance as yf
 import pandas as pd
 import re
